ea7a458aafabfb2881c3bc98f694ac681ec8be4b
JFCiscoHuerta <151865620+JFCiscoHuerta@users.noreply.github.com>
2025-05-23 21:45:14 -0600

    Initial Commit

---
+++ src/main/java/com/gklyphon/AnswerQ/AnswerQApplication.java
@@ -0,0 +1,13 @@
+package com.gklyphon.AnswerQ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnswerQApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnswerQApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gklyphon/AnswerQ/AnswerQApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gklyphon.AnswerQ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnswerQApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
