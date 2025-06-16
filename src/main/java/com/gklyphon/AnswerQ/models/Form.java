106899080bf278b2a8d512951a8e6c51ca4f2b86
JFCiscoHuerta <151865620+JFCiscoHuerta@users.noreply.github.com>
2025-06-15 19:42:31 -0600

    Replace Lombok Annotations

--- src/main/java/com/gklyphon/AnswerQ/models/Form.java
+++ src/main/java/com/gklyphon/AnswerQ/models/Form.java
@@ -1,18 +1,11 @@
 package com.gklyphon.AnswerQ.models;
 
 import jakarta.persistence.*;
-import lombok.*;
 
 import java.util.Set;
 
-
-@Builder
-@Getter
-@Setter
 @Entity
 @Table
-@AllArgsConstructor
-@NoArgsConstructor
 public class Form {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,6 +14,69 @@ public class Form {
     private boolean isEnabled;
     private String pin;
 
+    @ManyToOne
+    private User user;
+
     @OneToMany(mappedBy = "form", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<Question> questions;
+
+    public Form() {
+    }
+
+    public Form(Long id, String name, boolean isEnabled, String pin, User user, Set<Question> questions) {
+        this.id = id;
+        this.name = name;
+        this.isEnabled = isEnabled;
+        this.pin = pin;
+        this.user = user;
+        this.questions = questions;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isEnabled() {
+        return isEnabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        isEnabled = enabled;
+    }
+
+    public String getPin() {
+        return pin;
+    }
+
+    public void setPin(String pin) {
+        this.pin = pin;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Set<Question> getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(Set<Question> questions) {
+        this.questions = questions;
+    }
 }
