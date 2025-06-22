110ea5dd438505638e792e78ce522f5cdb8dff5a
JFCiscoHuerta <151865620+JFCiscoHuerta@users.noreply.github.com>
2025-06-21 22:36:08 -0600

    Method Argument Exception Status

--- src/main/java/com/gklyphon/AnswerQ/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/gklyphon/AnswerQ/exceptions/GlobalExceptionHandler.java
@@ -4,11 +4,28 @@ import com.gklyphon.AnswerQ.exceptions.exception.ElementNotFoundException;
 import org.hibernate.service.spi.ServiceException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getAllErrors().forEach((err) -> {
+            String field = ((FieldError) err).getField();
+            String message = err.getDefaultMessage();
+            errors.put(field, message);
+        });
+        return ResponseEntity.badRequest().body(errors);
+    }
 
     @ExceptionHandler(ElementNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleElementNotFoundException(ElementNotFoundException ex) {
