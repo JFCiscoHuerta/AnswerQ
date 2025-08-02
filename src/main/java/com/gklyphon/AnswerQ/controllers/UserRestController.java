019a1a5c662a9a1df5fbd44fbeb7d57e8d0c1129
JFCiscoHuerta <151865620+JFCiscoHuerta@users.noreply.github.com>
2025-08-01 23:23:06 -0600

    Add UserRestController Class

---
+++ src/main/java/com/gklyphon/AnswerQ/controllers/UserRestController.java
@@ -0,0 +1,31 @@
+package com.gklyphon.AnswerQ.controllers;
+
+import com.gklyphon.AnswerQ.dtos.EmailUpdateDto;
+import com.gklyphon.AnswerQ.dtos.PasswordUpdateDto;
+import com.gklyphon.AnswerQ.exceptions.exception.ElementNotFoundException;
+import com.gklyphon.AnswerQ.services.profile.ProfileService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v1/users")
+public class UserRestController {
+
+    private final ProfileService profileService;
+
+    public UserRestController(ProfileService profileService) {
+        this.profileService = profileService;
+    }
+
+    @PutMapping("/change-email/{id}")
+    public ResponseEntity<?> changeEmail(@PathVariable Long id, @RequestBody EmailUpdateDto emailUpdateDto) throws ElementNotFoundException {
+        profileService.updateEmail(id, emailUpdateDto);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/change-password/{id}")
+    public ResponseEntity<?> changePassword(@PathVariable Long id, @RequestBody PasswordUpdateDto passwordUpdateDto) throws ElementNotFoundException {
+        profileService.updatePassword(id, passwordUpdateDto);
+        return ResponseEntity.ok().build();
+    }
+}
