30e7d54f254681e26baee30e72ba13745b18c65b
JFCiscoHuerta <151865620+JFCiscoHuerta@users.noreply.github.com>
2025-11-23 23:43:21 -0600

    Add InvalidCredentialsException class

---
+++ src/main/java/com/gklyphon/AnswerQ/exceptions/exception/InvalidCredentialsException.java
@@ -0,0 +1,7 @@
+package com.gklyphon.AnswerQ.exceptions.exception;
+
+public class InvalidCredentialsException extends RuntimeException {
+    public InvalidCredentialsException(String message) {
+        super(message);
+    }
+}
